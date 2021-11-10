78fa8cde8495e26752e1db05c480a3a63369b4ae
David Larronda <david.larronda@gmail.com>
2021-11-10 13:14:51 -0300

    link whatsapp

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
           <p class="text-red-900 font-medium">
             Desenvolvedor
           </p>
-          <button >Mensagem</button>
+          <button ><a href="https://api.whatsapp.com/send?phone=5511973849522">Mensagem</a></button>
           {/* <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button> */}
           </div>
           
